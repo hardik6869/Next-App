94eb01f5935d8f9c6f6f471b9773b11e85da2c6c
hardik <hardik@gmail.com>
2022-05-13 11:35:57 +0530

    Add: Featcher

---
+++ lib/mutations.ts
@@ -0,0 +1,9 @@
+import Fetcher from "./fetcher";
+import featcher from "./fetcher";
+
+export const auth = (
+  mode: "signin | signup",
+  body: { email: string; password: string }
+) => {
+  return Fetcher(`/${mode}`, body);
+};
