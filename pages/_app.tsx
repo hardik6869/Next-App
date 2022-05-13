8bc2c15fda1c20a7dc61f55000927c2a19ce03fd
hardik <hardik@gmail.com>
2022-05-13 14:30:42 +0530

    Auth Form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,9 +32,13 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <PlayLayout>
+      {Component.authPage ? (
         <Component {...pageProps} />
-      </PlayLayout>
+      ) : (
+        <PlayLayout>
+          <Component {...pageProps} />
+        </PlayLayout>
+      )}
     </ChakraProvider>
   );
 }
